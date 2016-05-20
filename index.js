9867c2ec2951a62ee5b61b79282dd3d0a27f9e0d
Peter Graham <peterghm@gmail.com>
2016-05-19 23:29:46 -0400

    basic endpoint

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 
 require('dotenv').config({silent: true})
 const token = process.env.SLACK_COMMAND_TOKEN
+const port = process.env.PORT || '3000'
+const express = require('express')
+let app = express()
+
+app.post('/graph', function (req, res, next) {
+  if (req.params.token !== token) {
+    return res.status(400).send('Invalid token').end()
+  } else if (!req.params.text || req.params.text === "") {
+    return res.status(400).send('Must specify some numbers').end()
+  }
+  res.send('it works :shipit: ' + req.params.text)
+})
+
+console.log("Listening on port: " + port)
+app.listen(port)
