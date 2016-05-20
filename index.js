4304b85eae4826653a6499528602e0e6c87d2c03
Peter Graham <peterghm@gmail.com>
2016-05-19 23:11:58 -0400

    uninstall slack client, only need slack command bot

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 'use strict'
 
-const slack = require('@slack/client')
-const RtmClient = slack.RtmClient
-const CLIENT_EVENTS = slack.CLIENT_EVENTS
 require('dotenv').config({silent: true})
-
-const token = process.env.SLACK_API_TOKEN
-
-let rtm = new RtmClient(token, {logLevel: 'debug'})
-rtm.start()
+const token = process.env.SLACK_COMMAND_TOKEN
