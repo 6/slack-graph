7f0a7c56d15fc3bc75f2536268fe5c4d29820d04
Peter Graham <peterghm@gmail.com>
2016-05-19 23:58:34 -0400

    try sparkly

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const token = process.env.SLACK_COMMAND_TOKEN
 const port = process.env.PORT || '3000'
 const express = require('express')
 const bodyParser = require('body-parser')
+const chart = require('ascii-chart')
+const sparkly = require('sparkly')
 
 let app = express()
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/graph', function (req, res, next) {
   if (req.params.token !== token) {
@@ -15,7 +17,8 @@ app.post('/graph', function (req, res, next) {
   } else if (!req.body || !req.body.text) {
     return res.status(400).send('Must specify some numbers').end()
   }
-  res.send('it works :shipit: ' + req.body.text)
+  let data = req.body.text.split(',')
+  res.send(sparkly(data))
 })
 
 console.log("Listening on port: " + port)
